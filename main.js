5fb3debf47e1a373e24964377000d5fdc0260779
luzhihong <xiaopikaqiu@outlook.com>
2025-02-24 11:00:39 +0800

    init project

---
+++ main.js
@@ -0,0 +1,16 @@
+import { app, BrowserWindow } from "electron";
+import isDev from "electron-is-dev";
+
+let mainWindow;
+
+app.on("ready", () => {
+  mainWindow = new BrowserWindow({
+    width: 1024,
+    height: 680,
+    webPreferences: {
+      nodeIntegration: true,
+    },
+  });
+  const urlLocation = isDev ? "http://localhost:3000" : "https://baidu.com";
+  mainWindow.loadURL(urlLocation);
+});
